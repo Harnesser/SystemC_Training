79c5be67efe818d824d5251bb751ebc6c30bdcf3
Harnesser <harnesser+lt@gmail.com>
2011-07-07 03:06:23 +0100

    Starting uni_string_rep

---
+++ ch03/uni_string_rep/uni_string_rep.cpp
@@ -0,0 +1,18 @@
+/*
+ * SystemC's Unified String Representations'
+ */
+
+#define SC_INCLUDE_FX
+#include "systemc.h"
+#include <iomanip>
+#include <string>
+
+using namespace std;
+using namespace sc_dt;
+
+int main(int argc, char *argv[] )
+{
+	
+	cout << "INFO: Starting uni_string_rep" << endl;
+	
+}
