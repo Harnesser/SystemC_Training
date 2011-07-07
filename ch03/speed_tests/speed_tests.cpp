32d5a4fa0cc8dd9ba538a188cc6101b8cbd8b425
Harnesser <harnesser+lt@gmail.com>
2011-07-08 00:37:31 +0100

    Starting speed test code

---
+++ ch03/speed_tests/speed_tests.cpp
@@ -0,0 +1,19 @@
+/*
+ * Speed test
+ * Define the data type on the Makefile command line
+ */
+
+#ifndef ST_DATATYPE
+	#define ST_DATATYPE this_will_break
+#endif
+
+#define SC_INCLUDE_FX
+#include "systemc.h"
+
+int main( int argc, char *argv [] ) 
+{
+	cout << "Marty" << endl;
+	
+}
+
+
