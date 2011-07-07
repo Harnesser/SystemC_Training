f0faf543c630bd37c78889bea78cab9aa96d66f7
Harnesser <harnesser+lt@gmail.com>
2011-07-07 04:52:12 +0100

    Finished Ex3.2, but with no error checking

--- ch03/file_reader/file_reader.cpp
+++ ch03/file_reader/file_reader.cpp
@@ -6,6 +6,9 @@
  * values as SC_DEC and SC_HEX_SM.
  */
 
+// How much data?
+// Just integers?
+
 #include "systemc.h"
 
 #include <fstream>
@@ -13,18 +16,33 @@
 
 using std::string;
 
+#define ARRAY_SIZE 10
+#define INT_SIZE 12
+
 int main(int argc, char *argv[] )
 {
+	sc_uint<INT_SIZE> numbers [ARRAY_SIZE];
+	
+	// Read numbers from a file.
 	ifstream infile("numbers.data");
 	if( !infile ) {
 		cerr << "Unable to read file numbers.data" << endl;
 		exit(1);
 	}
 
-	string first_line;
-	infile>>first_line;
-
-	cout << "First Line:" << first_line << endl;
-
+	int i = 0;
+	for( i=0; i < ARRAY_SIZE; i++ ) {
+		infile >> numbers[i];
+	}
 	infile.close();
+	
+	// Print out what we've got
+	cout << "Read  " << i << " numbers." << endl;
+	while( --i >= 0 ) {
+		cout << "  [" << i << "]"
+			<< " " << numbers[i].to_string(SC_DEC)
+			<< " " << numbers[i].to_string(SC_HEX_SM)
+			<< endl;
+	}
+
 }
