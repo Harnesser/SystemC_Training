758df930126bbc8b76cdd30db0248078a1bce636
Harnesser <harnesser+lt@gmail.com>
2011-07-07 03:25:32 +0100

    Finished unified string repr

--- ch03/uni_string_rep/uni_string_rep.cpp
+++ ch03/uni_string_rep/uni_string_rep.cpp
@@ -12,7 +12,52 @@ using namespace sc_dt;
 
 int main(int argc, char *argv[] )
 {
-	
 	cout << "INFO: Starting uni_string_rep" << endl;
 	
+	sc_numrep NR[] = {
+		SC_NOBASE,
+		SC_DEC,
+		SC_BIN,
+		SC_BIN_US,
+		SC_BIN_SM,
+		SC_OCT,
+		SC_OCT_US,
+		SC_OCT_SM,
+		SC_HEX,
+		SC_HEX_US,
+		SC_HEX_SM,      // Signed magnitude?
+		SC_CSD			// Can this really be canonical-signed-digit?
+	};
+		
+	sc_int<5> INT[] = { -13, -3, -1, 0, 1, 3, 13 };
+	sc_fixed<7,3> FXD[] = { -13.3, -2.7, -1.0, 0.0, 1.0, 3.3, 13.7 };
+	sc_fixed<7,3> FSD[] = { "-13.3", "-2.7", "-1.0", "0.0", "1.0",
+		"3.3", "13.7" };
+		
+	string q("'");
+	
+	for( unsigned iTC=0; iTC != 7; iTC++ )
+		for( unsigned iNR=1; iNR != 12; iNR++ ) 
+		{
+			cout << "sc_int       " << std::setw(7) << INT[iTC] 
+				 << " output as "   << std::setw(9) << NR[iNR]
+				 << " = "           << std::setw(15) 
+				 << q + INT[iTC].to_string(NR[iNR], true ) + q
+				 << std::endl;
+				 
+			cout << "sc_fixed     " << std::setw(7) << FXD[iTC]
+				 << " output as "   << std::setw(9) << NR[iNR]
+				 << " = "           << std::setw(15) 
+				 << q + FXD[iTC].to_string(NR[iNR], true ) + q
+				 << std::endl;
+				 
+			cout << "sc_fixed str " << std::setw(7) << FSD[iTC]
+				 << " output as "   << std::setw(9) << NR[iNR]
+				 << " = "           << std::setw(15)
+				 << q + FSD[iTC].to_string(NR[iNR], true ) + q
+				 << std::endl;
+		}
+
+	cout << "INFO: uni_string_rep COMPLETED" << endl;
+	return 0;
 }
