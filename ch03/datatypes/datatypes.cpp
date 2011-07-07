a7f5c00b6d77dc28e2af8226787dd4fda7c4bbcc
Harnesser <harnesser+lt@gmail.com>
2011-07-07 02:04:31 +0100

    Starting datatypes exercise

---
+++ ch03/datatypes/datatypes.cpp
@@ -0,0 +1,19 @@
+#define SC_INCLUDE_FX
+#include "systemc.h"
+
+#include <string>
+#include <bitset>
+#include <iomanip>
+
+using std::cout;
+using std::endl;
+using std::string;
+using std::bitset;
+using std::setw;
+
+using namespace sc_dt;
+
+int main(int argc, char* argv[]) 
+{
+	cout << "INFO: Starting datatypes" << endl;
+}	
