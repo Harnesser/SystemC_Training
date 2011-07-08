410b353a1aefa3df4e063c0516a84bec33563332
Harnesser <harnesser+lt@gmail.com>
2011-07-08 02:28:24 +0100

    Some sinewave groundwork

--- ch03/accuracy/accuracy.cpp
+++ ch03/accuracy/accuracy.cpp
@@ -8,12 +8,43 @@
  * period.  Assume you are limited to 12 bits for both the input and
  * the output.  Try different modes. HINT: Start modelling using double.
  * Progress to using sc_fixed.
+ *
+ * NOTES:
+ *  I'm thinking of generating the sinewaves using doubles, then
+ * converting them to sc_fixed, and printing both out to a file in
+ * CSV format so I can have a peek in soffice? Or compute the errors?
+ * I'm not 100% sure what I'm to be doing here...
  */
 
 #define SC_INCLUDE_FX
 #include "systemc.h"
 
+#define NUM_POINTS 512
+double wave_1[NUM_POINTS];
+
+void gen_wave( double wave[], double freq, double amp);
+void show_wave( double wave[] );
+ 
 int main(int argc, char *argv[] )
 {
 	cout << "Hello!" << endl;
+	gen_wave(wave_1, 22, 3000);
+	show_wave(wave_1);
+}
+
+void gen_wave( double wave[], double freq, double amp)
+{
+	for(int i=0; i<NUM_POINTS; i++ ) {
+		wave[i] = i;
+	}
+}
+
+void show_wave( double wave[] )
+{
+	for(int i=0; i<NUM_POINTS; i++ ) {
+		cout << wave[i] << " ";
+		if( i % 30 == 0 ) {
+			cout << endl;
+		}
+	}
 }
