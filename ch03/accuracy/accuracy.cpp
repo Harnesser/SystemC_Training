bbf920e55e4ec4da167b7e5a46fde92fe2901c9f
Harnesser <harnesser+lt@gmail.com>
2011-07-08 02:47:52 +0100

    Some sinewaving

--- ch03/accuracy/accuracy.cpp
+++ ch03/accuracy/accuracy.cpp
@@ -19,7 +19,15 @@
 #define SC_INCLUDE_FX
 #include "systemc.h"
 
+#include <cmath>
+
 #define NUM_POINTS 512
+double amp_1 = 14;
+double freq_1 = 345;
+double amp_2 = 22;
+double freq_2 = 2.5 * freq_1;
+double t_delta = ( 1.0 / freq_1 ) / NUM_POINTS;
+
 double wave_1[NUM_POINTS];
 
 void gen_wave( double wave[], double freq, double amp);
@@ -28,14 +36,18 @@ void show_wave( double wave[] );
 int main(int argc, char *argv[] )
 {
 	cout << "Hello!" << endl;
-	gen_wave(wave_1, 22, 3000);
+	gen_wave(wave_1, amp_1, freq_1);
 	show_wave(wave_1);
+	cout << "PI " << M_PI << endl;
+    cout << "PI/2 " << M_PI_2 << endl;
+    cout << "2*PI (not) " << M_2_PI << endl; //nope, it's not...
+
 }
 
-void gen_wave( double wave[], double freq, double amp)
+void gen_wave( double wave[], double amp, double freq)
 {
 	for(int i=0; i<NUM_POINTS; i++ ) {
-		wave[i] = i;
+		wave[i] = amp * sin( 2.0 * M_PI * freq * (t_delta * i ));
 	}
 }
 
@@ -47,4 +59,5 @@ void show_wave( double wave[] )
 			cout << endl;
 		}
 	}
+	cout << endl;
 }
